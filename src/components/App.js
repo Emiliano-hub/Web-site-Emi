2a63d1c3f5c704523750303f12ec0229c8a02a0c
EMI <emiliano.2.farias@hotmail.com>
2021-07-26 16:58:04 -0300

    Hero 1Version

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+import Hero from './Hero';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Hero />
+    </React.Fragment>
+  );
+}
+
+export default App;
